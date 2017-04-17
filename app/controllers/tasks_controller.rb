1797a3f54cf52b214dd2d1ebb4ac122d6420e04d
aiueo <magicnightt@yahoo.co.jp>
2017-04-17 07:52:39 +0000

    revise status column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,49 +1,67 @@
 class TasksController < ApplicationController
-  def index
-	@tasks = Task.all   end
-  def show
-	@task = Task.find(params[:id])  end
-  def new
-	@task = Task.new  end
-  def create
-    @task = Task.new(task_params)
-    if @task.save
-      flash[:success] = '新規タスクが正常に投稿されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = '新規タスクが投稿されませんでした'
-      render :new
-    end
-  end
-  def edit
-	@task = Task.find(params[:id])  end
-def update
-      @task = Task.find(params[:id])
+  
+   def index
+ 	  @tasks = Task.all   end
+ 	  
+   def show
+ 	  @task = Task.find(params[:id])  end
+ 	  
+   def new
+   	@task = Task.new
+   	@stat = Task.new
+	end
+   	
+ 	
+   def create
+     @task = Task.new(task_params)
 
-    if @task.update(task_params)
-      flash[:success] = 'タスク は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'タスク は更新されませんでした'
-      render :edit
-    end
-  end  
+     if @task.save
+       flash[:success] = '新規タスクが正常に投稿されました'
+       redirect_to @task
+     else
+       flash.now[:danger] = '新規タスクが投稿されませんでした'
+       render :new
+     end
+   end
+   
+   
+   def edit
+ 	  @task = Task.find(params[:id])
+ 	end
+ 	
+ 	
+ def update
+       @task = Task.find(params[:id])
+#       @stat = Task.find(params[:id])
 
-  def destroy
-    @task = Task.find(params[:id])
-    @task.destroy
-
-    flash[:success] = 'タスク は正常に削除されました'
-    redirect_to tasks_url
-  end
-
-end
-
-
-
-  private
-
-  # Strong Parameter
-  def task_params
-    params.require(:task).permit(:content)
-  end
+     if @task.update(task_params)
+       flash[:success] = 'タスク は正常に更新されました'
+       redirect_to @task
+     else
+       flash.now[:danger] = 'タスク は更新されませんでした'
+       render :edit
+     end
+   end  
+ 
+   def destroy
+     @task = Task.find(params[:id])
+     @task.destroy
+ 
+     flash[:success] = 'タスク は正常に削除されました'
+     redirect_to tasks_url
+   end
+ 
+ end
+ 
+ 
+ 
+   private
+ 
+   # Strong Parameter
+   def task_params
+     params.require(:task).permit(:content, :status)
+   end
+   
+  # def task_stat
+  #   params.require(:task).permit(:status)
+  # end
